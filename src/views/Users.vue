c5e745a79b91897ef9d0b78888dc0d22f31db690
99Botch <yvanmonod@protonmail.com>
2022-02-13 18:12:55 +0100

    GET users parge implemented

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="users">
+  <div class="users" id="bite">
     <p>This is the users' page</p>
+    {{ info }}
   </div>
+  
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'bite',
+  data () {
+    return {
+      info: null
+    }
+  },
+mounted () {
+    axios
+      .get('http://localhost:3000/users/')
+        .then(response => (this.info = response.data))
+        .catch(e => { this.errors.push(e) })
+  }
+}
+</script>
