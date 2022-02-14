6b93fb50362c244c4c457fa5dc7ed2509dbfb55e
99Botch <yvanmonod@protonmail.com>
2022-02-14 15:04:12 +0100

    front-end register works

--- src/views/users/Register.vue
+++ src/views/users/Register.vue
@@ -4,32 +4,68 @@
         <p>Register form</p>
 
         <div>
+            <form>
+                <label for="username">Username:</label><br>
+                <input type="text" id="username" name="username" v-model="form.username" required><br>                
+                
+                <label for="email">Email:</label><br>
+                <input type="text" id="email" name="email" v-model="form.email" required><br>
+                
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password" v-model="form.password" required><br>
+                
+                <label for="repeat_password">Repeat password:</label><br>
+                <input type="password" id="repeat_password" name="repeat_password" v-model="form.repeat_password" required><br>
+
+                <br>
+            </form>
             
+            <div class="button">
+                <button class="submit" type="submit" @click="submitForm">Sign up</button>
+            </div>
         </div>
 
     </div>
 </template>
 
 <script>
-    // import axios from 'axios';
-    // import { URI } from '@/plugins/url.js';
+    import axios from 'axios';
+    import { URI } from '@/plugins/url.js';
 
     export default {
         name: 'Register',
         
         data () {
             return {
-                
+                form: {
+                    username: "",
+                    email: "",
+                    password: "",
+                    repeat_password: ""
+                }
             }
         },
 
         mounted () {
-            this.register();
+            this.submitForm();
         },
 
         methods: {
-            register(){
+            async submitForm(){
+                const json = JSON.stringify(this.form);
+                console.log(json);
 
+                await axios.post(`${ URI }/users/register`, json, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then((res) => {
+                    console.log(res)
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
             }
         }
     }
