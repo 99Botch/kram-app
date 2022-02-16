dce313a5823a9ca0b47bb504280071d84483d5f0
99Botch <yvanmonod@protonmail.com>
2022-02-16 22:42:21 +0100

    signout page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
 const routes = [
     {
@@ -12,33 +12,38 @@ const routes = [
         name: 'PageNotFound',
         /* route level code-splitting: this generates a separate chunk (about.[hash].js) for this route
         which is lazy-loaded when the route is visited. */
-        component: () => import('../components/PageNotFound.vue')
+        component: () => import('@/components/PageNotFound.vue')
+    },
+    {
+        path: '/logout',
+        name: 'Signout',
+        component: () => import('@/views/Signout.vue')
     },
     {
         path: '/users',
         name: 'Users',
-        component: () => import('../views/users/Users.vue')
+        component: () => import('@/views/users/Users.vue')
     },
     {
         path: '/users/:id',
         name: 'User',
         props: true,
-        component: () => import('../views/users/User.vue')
+        component: () => import('@/views/users/User.vue')
     },
     {
         path: '/register',
         name: 'Register',
-        component: () => import('../views/users/Register.vue')
+        component: () => import('@/views/users/Register.vue')
     },
     {
         path: '/login',
         name: 'Login',
-        component: () => import('../views/Login.vue')
+        component: () => import('@/views/Login.vue')
     },
     {
         path: '/edit/:id',
         name: 'EditUser',
-        component: () => import('../views/users/EditUser.vue')
+        component: () => import('@/views/users/EditUser.vue')
     }
 ]
 
---
+++ src/views/Signout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="" id="signout">
+        <h1>Signout</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Signout',
+        
+        data () {
+            return {}
+        },
+
+        mounted () {},
+
+        methods: {}
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
