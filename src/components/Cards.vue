aa501efea9b04c178db3dbb0a000af6355fac9ff
99Botch <yvanmonod@protonmail.com>
2022-03-27 12:56:13 +0200

    add card to deck first step

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -5,17 +5,35 @@
 
         <AddCard @clicked="renderCard"/>
 
-        <div v-for="card of cards" :key="card.id" >
-            <div>            
-                <h5>Question</h5>
-                <p> {{ card.question }}</p>
-            </div>
+        <div>
+            <div>
 
-            <div>            
-                <h5>Answer</h5>
-                <p> {{ card.answer }}</p>
             </div>
-            <hr />
+            
+            <table>
+                <tr>
+                    <th>Question</th>
+                    <th>Answer</th>
+                    <th>Fail Counter</th>
+                    <th>Burry</th>
+                    <th>Add/remove</th>
+                </tr>
+                <tr v-for="card of cards" :key="card.id" >
+                    <td>{{ card.question }}</td>
+                    <td>{{ card.answer }}</td>
+                    <td>X</td>
+                    <td>X</td>
+                    <td>
+                        <p  @click="revealCollapse" :id="card._id">Collapse</p>
+                        <div class="collapse-belong" >
+                            <span v-for="deck of decks" :key="deck.deck_id" >
+                                <input type="checkbox" :id="deck.deck_id" @click="cardToDeck(card._id)"/>
+                                {{deck.name}}
+                            </span>
+                        </div>
+                    </td>
+                </tr>
+            </table>
         </div>
 
     </span>
@@ -38,10 +56,12 @@
         data() {
             return {
                 cards: [],
+                decks: [],
                 id: localStorage.getItem('_id'),
                 deck_id: this.$store.getters.deckCardsId,
                 saved: false,
-                loading: true
+                loading: true,
+                collapse: false
             }
         },
 
@@ -52,12 +72,52 @@
         computed: {},
 
         methods: {
+            revealCollapse(){
+                let display = event.target.nextElementSibling.style.display;
+                (display == '') ? event.target.nextElementSibling.style.display= 'flex' : 
+                    event.target.nextElementSibling.style.display= '';
+            },
+            
+            async cardToDeck(_id){
+                console.log(event.target)
+                console.log(event.target.id)
+                console.log(_id)
+
+                const json = JSON.stringify({
+                    deck_id: event.target.id,
+                    card_id: _id
+                });
+
+                await axios.put(`${ URI }/cards/add-to-deck/${ this.id }`, json, {
+                    headers: {
+                        Authorization: `Bearer ${ localStorage.getItem('token') }`,
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then((res) => {
+                    if(res.status === 200) {
+                        console.log(res)
+                    }
+                })
+                .catch((error) => {
+                    [this.form.question, this.form.answer] = '';
+                    if (error) this.formError = "Email or password invalid"
+                });
+            },
+
             async getCards(){
-                await axios.get( `${ URI }/cards/${ this.id }`, {
+                const promise1 = await axios.get( `${ URI }/cards/${ this.id }`, {
                     headers: { Authorization: `Bearer ${ localStorage.getItem('token') }` }
-                    })
-                    .then(response => {
-                        this.cards = response.data;
+                })
+
+                const promise2 = await axios.get( `${ URI }/decks/table/${ this.id }`, {
+                    headers: { Authorization: `Bearer ${ localStorage.getItem('token') }` }
+                })
+
+                Promise.all([promise1, promise2])
+                    .then(async values => {
+                        this.cards = await values[0].data;
+                        this.decks = await values[1].data;
                         this.loading = false;
                     })
                     .catch(err => { console.log(err) })
@@ -90,4 +150,11 @@
     }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+    .collapse-belong{
+        display: none;
+        position: absolute;
+        flex-direction: column;
+        background-color: aquamarine;
+    }
+</style>
